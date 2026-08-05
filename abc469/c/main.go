92daf77f8226507192a87d3d98a4c643af2ba201
mo-taki <ewok.mfk@gmail.com>
2026-08-05 23:06:58 +0900

    c_カンニングしました。

---
+++ abc469/c/main.go
@@ -0,0 +1,68 @@
+// https://atcoder.jp/contests/abc469/tasks/abc469_c
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer wr.Flush()
+	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
+	sc.Split(bufio.ScanWords)
+
+	n := ni()
+	s := ns()
+
+	var idxs = []int{}
+	for i, v := range s {
+		if v == 'x' {
+			idxs = append(idxs, i)
+		}
+	}
+
+	for i := range n {
+		if len(idxs) < i+1 {
+			fmt.Fprintln(wr, n)
+		} else {
+			fmt.Fprintln(wr, idxs[i]+1)
+		}
+	}
+
+}
+
+func ns() string  { sc.Scan(); return sc.Text() }
+func ni() int     { i, _ := strconv.Atoi(ns()); return i }
+func nf() float64 { f, _ := strconv.ParseFloat(ns(), 64); return f }
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := range a {
+		a[i] = ni()
+	}
+	return a
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func digitSum(x int) int {
+	s := 0
+	for ; x > 0; x /= 10 {
+		s += x % 10
+	}
+	return s
+}
+
+func isEven(x int) bool {
+	return x%2 == 0
+}
