6fe191033786443e9feb4c5aaac6ef2b29a13b6e
mo-taki <ewok.mfk@gmail.com>
2026-08-11 23:09:31 +0900

    a

---
+++ abc466/a/main.go
@@ -0,0 +1,58 @@
+// 問題URL: https://atcoder.jp/contests/abc466/tasks/abc466_a
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer wr.Flush()
+	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
+	sc.Split(bufio.ScanWords)
+
+	n := ni()
+	ans := "Yes"
+
+	for range n {
+		x := ni()
+		if x >= 0 {
+			ans = "No"
+		}
+	}
+
+	fmt.Fprintln(wr, ans)
+}
+
+func ns() string  { sc.Scan(); return sc.Text() }
+func ni() int     { i, _ := strconv.Atoi(ns()); return i }
+func nf() float64 { f, _ := strconv.ParseFloat(ns(), 64); return f }
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := range a {
+		a[i] = ni()
+	}
+	return a
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+func digitSum(x int) int {
+	s := 0
+	for ; x > 0; x /= 10 {
+		s += x % 10
+	}
+	return s
+}
+func isEven(x int) bool {
+	return x%2 == 0
+}
